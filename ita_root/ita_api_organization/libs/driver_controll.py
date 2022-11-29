b699d5571ca10e048951e59f668bac3cd4730bb8
enoman <c_man_enomoto@ncontr.com>
2022-11-29 20:19:23 +0900

    #434

--- ita_root/ita_api_organization/libs/driver_controll.py
+++ ita_root/ita_api_organization/libs/driver_controll.py
@@ -126,7 +126,7 @@ def get_execution_info(objdbca, target, execution_no):
         RETRUN:
             data
     """
-    
+
     # 該当の作業管理を取得(ID変換のためloadTableで取得)
     objmenu = load_table.loadTable(objdbca, target['execution_list'])
     filter_parameter = {
@@ -193,7 +193,7 @@ def get_execution_info(objdbca, target, execution_no):
         execution_info['number_of_rows_to_display_progress_status'] = tmp_row['ANSIBLE_TAILLOG_LINES']
 
     return execution_info
-    
+
 def reserve_cancel(objdbca, execution_no):
     """
         予約取消
@@ -204,18 +204,18 @@ def reserve_cancel(objdbca, execution_no):
             status: SUCCEED
             execution_no: 作業No
     """
-    
+
     # 該当の作業実行のステータス取得
     where = "WHERE EXECUTION_NO = %s"
     objdbca.table_lock(["T_ANSR_EXEC_STS_INST"])
     data_list = objdbca.table_select("T_ANSR_EXEC_STS_INST", where, [execution_no])
-    
+
     # 該当する作業実行が存在しない
     if len(data_list) is None or len(data_list) == 0:
         log_msg_args = [execution_no]
         api_msg_args = [execution_no]
         raise AppException("499-00903", log_msg_args, api_msg_args)
-    
+
     for row in data_list:
         # ステータスが未実行(予約)でない
         if not row['STATUS_ID'] == AnscConst.RESERVE:
@@ -227,14 +227,15 @@ def reserve_cancel(objdbca, execution_no):
                     status = tmp_row['EXEC_STATUS_NAME_JA']
                 else:
                     status = tmp_row['EXEC_STATUS_NAME_EN']
-                
+
             log_msg_args = [status]
             api_msg_args = [status]
             raise AppException("499-00910", log_msg_args, api_msg_args)
-        
+
         # ステータスを予約取消に変更
         update_list = {'EXECUTION_NO': execution_no, 'STATUS_ID': AnscConst.RESERVE_CANCEL}
         ret = objdbca.table_update('T_ANSR_EXEC_STS_INST', update_list, 'EXECUTION_NO', True)
         objdbca.db_commit()
-        
-    return {"status": "SUCCEED", "execution_no": execution_no}
+
+    result_msg = g.appmsg.get_api_message("MSG-10904", [execution_no])
+    return result_msg

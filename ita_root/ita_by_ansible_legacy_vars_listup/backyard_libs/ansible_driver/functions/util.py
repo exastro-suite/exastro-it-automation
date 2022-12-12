f345f980b58ae92467161fc6536184c06ccbb22c
yoshiyuki-konuma <yoshiyuki-konuma@ncontr.com>
2022-12-12 16:47:45 +0900

    #70, #71 legacy/pioneer vars listup update

--- ita_root/ita_by_ansible_legacy_vars_listup/backyard_libs/ansible_driver/functions/util.py
+++ ita_root/ita_by_ansible_legacy_vars_listup/backyard_libs/ansible_driver/functions/util.py
@@ -82,7 +82,7 @@ def extract_variable_for_execute(mov_vars_dict, tpl_varmng_dict, device_varmng_d
 
     sub_value_auto_reg = SubValueAutoReg()
     driver_type = AnscConst.DF_LEGACY_DRIVER_ID
-    _, template_list, host_list = sub_value_auto_reg.get_data_from_all_parameter_sheet(WS_DB=ws_db)
+    _, template_list, host_list = sub_value_auto_reg.get_data_from_all_parameter_sheet(driver_type, WS_DB=ws_db)
     # template_list = { MovementID: { TPF変数名: 0 }, … }
     # host_list = { MovementID: { OPERATION_ID: { SYSTEM_ID: 0 }, … }, … }
 

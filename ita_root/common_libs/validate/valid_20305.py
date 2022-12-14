eb448828751e2dc628572b2d99eaaae614d35a81
m-kawata-git <m-kawata0831@ncontr.com>
2022-12-14 15:49:04 +0900

    #70 #71 Ansible Legacy/Pioneer個別バリデーション修正

--- ita_root/common_libs/validate/valid_20305.py
+++ ita_root/common_libs/validate/valid_20305.py
@@ -18,4 +18,4 @@ def external_valid_menu_before(objdbca, objtable, option):
     data_list = {"LOADED_FLG": "0", "ROW_ID": "203"}
     primary_key_name = "ROW_ID"
     objdbca.table_update(table_name, data_list, primary_key_name, False)
-    return retBool, msg, option,
+    return retBool, msg, option

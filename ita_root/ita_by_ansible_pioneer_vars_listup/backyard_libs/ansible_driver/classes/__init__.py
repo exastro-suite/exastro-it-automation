00c101561e381437b755e70e0b69812949fcfcd1
yoshiyuki-konuma <yoshiyuki-konuma@ncontr.com>
2023-02-20 16:59:31 +0900

    #71 削除したソースの参照消し忘れ

--- ita_root/ita_by_ansible_pioneer_vars_listup/backyard_libs/ansible_driver/classes/__init__.py
+++ ita_root/ita_by_ansible_pioneer_vars_listup/backyard_libs/ansible_driver/classes/__init__.py
@@ -4,5 +4,3 @@ from .MovementTableClass import MovementTable  # noqa F401
 from .MovementVarsLinkTableClass import MovementVarsLinkTable  # noqa F401
 from .TableBaseClass import TableBase  # noqa F401
 from .TemplateTableClass import TemplateTable  # noqa F401
-from .VariableClass import Variable  # noqa F401
-from .VariableManagerClass import VariableManager  # noqa F401

de001a84e91c4ae0e51b53bfb743844789252fd6
enoman <c_man_enomoto@ncontr.com>
2023-03-22 14:20:17 +0900

    #917

--- ita_root/ita_by_ansible_execute/a.py
+++
@@ -1,8 +0,0 @@
-import yaml
-yamlfile = 'a.yml'
-retParse = yaml.load(open(yamlfile).read(), Loader=yaml.FullLoader)
-print(retParse)
-#for key in retParse:
-#    print(key)
-#    if type(key) in (dict, list):
-#        print('error')

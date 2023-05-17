fcfd07809a00edcecbc7f5bdd94a30a21c8ba930
h-nanri <hibiki.nanri@lean-jp.com>
2023-05-17 16:56:50 +0900

    #1248, #1263

---
+++ ita_root/ita_migration/versions/2_1_1/WS_level/sql/terraform_cloud_ep_master.sql
@@ -0,0 +1,10 @@
+-- UPDATE: -2.1.1
+    -- T_COMN_MENU_COLUMN_LINK: UPDATE
+
+-- T_COMN_MENU_COLUMN_LINK:UPDATE
+UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_JA = 'Terraformと通信するプロトコルです。
+http/httpsのいずれかで、通常はhttpsです。', DESCRIPTION_EN = 'A protocol to communicate with Terraform.
+Either http/https, usually https.', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '8010102';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_JA = 'Terraformと通信するプロトコルです。
+http/httpsのいずれかで、通常はhttpsです。', DESCRIPTION_EN = 'A protocol to communicate with Terraform.
+Either http/https, usually https.', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '8010102';

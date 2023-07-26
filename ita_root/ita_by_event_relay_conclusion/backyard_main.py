13010cfbeee843dae6da36cb5e3563a8321dc25f
uekawa-tomonari <16901713+uekawa-tomonari@users.noreply.github.com>
2023-07-26 22:35:25 +0900

    イベント連携機能のコンテナを二つに

---
+++ ita_root/ita_by_event_relay_conclusion/backyard_main.py
@@ -0,0 +1,61 @@
+# Copyright 2022 NEC Corporation#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from flask import g
+from common_libs.common.dbconnect import *  # noqa: F403
+from common_libs.common.mongoconnect.mongoconnect import MONGOConnectWs
+from common_libs.common.util import ky_decrypt, get_timestamp, get_all_execution_limit, get_org_execution_limit
+from common_libs.ci.util import log_err
+
+# from libs import common_functions as cm
+
+
+def backyard_main(organization_id, workspace_id):
+    """
+    [ita_by_ansible_execute]
+    main logicのラッパー
+    called 実行君
+    """
+    g.applogger.debug(g.appmsg.get_log_message("BKY-00001"))
+
+    retBool = main_logic(organization_id, workspace_id)
+    if retBool is True:
+        # 正常終了
+        g.applogger.debug(g.appmsg.get_log_message("BKY-00002"))
+    else:
+        g.applogger.debug(g.appmsg.get_log_message("BKY-00003"))
+
+
+def main_logic(organization_id, workspace_id):
+    """
+    main logic
+    """
+    g.applogger.debug("organization_id=" + organization_id)
+    g.applogger.debug("workspace_id=" + workspace_id)
+    g.applogger.debug("WSMONGO_PASSWORD=" + ky_decrypt(g.db_connect_info["WSMONGO_PASSWORD"]))
+
+    wsMong = MONGOConnectWs()
+    g.applogger.debug("mongodb-ws can connet")
+
+    test_collection = wsMong.collection("test_collection")
+    # test_collection.insert_many([
+    #     {'名前': '太郎', '住所': '東京'},
+    #     {'名前': '次郎', '住所': '千葉'}
+    # ])
+    data_list = test_collection.find()
+    for data in data_list:
+        g.applogger.debug(data)
+
+    return True

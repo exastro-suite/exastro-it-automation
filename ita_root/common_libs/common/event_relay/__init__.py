939ba43d5a0e1e1d21e7a6e9cbc4daaff7782dce
yutoyajima <yuto-yajima@ncontr.com>
2023-08-10 17:13:24 +0900

    common_libsディレクトリ構造修正

--- ita_root/common_libs/common/event_relay/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api_client_common import APIClientCommon  # noqa: F401
-from .auth_method.auth_basic import BasicAuthAPIClient  # noqa: F401
-from .auth_method.auth_bearer import BearerAuthAPIClient  # noqa: F401
-from .auth_method.auth_sharedKeyLite import ShareKeyLiteAuthAPIClient  # noqa: F401

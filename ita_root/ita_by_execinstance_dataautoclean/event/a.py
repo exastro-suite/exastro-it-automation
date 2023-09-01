32a980d4ff8c814e97d7e8f926d79f5594f962f7
enoman <c_man_enomoto@ncontr.com>
2023-09-01 20:17:10 +0900

    #1507

--- ita_root/ita_by_execinstance_dataautoclean/event/a.py
+++
@@ -1,8 +0,0 @@
-def call(dat):
-    dat['bb'] = 'cc'
-    dic['aa']= 'Rdduu'
-
-dic = {}
-dic['aa']= 'uu'
-call(dic)
-print(dic)
--- ita_root/ita_by_execinstance_dataautoclean/event/csv/a.php
+++
@@ -1,6 +0,0 @@
-import json
-aa = []
-aa.append({"i_01_name": "down"})
-aa.append({"hostname": "ap01"})
-print(aa)
-print(json.dumps(aa))

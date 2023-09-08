2ef4c5e6800fe1183433f1f65b0aae6b839587ab
yutoyajima <yuto-yajima@ncontr.com>
2023-09-08 18:59:15 +0900

    イベント収集クラス修正

--- ita_root/common_libs/event_relay/__init__.py
+++ ita_root/common_libs/event_relay/__init__.py
@@ -2,3 +2,4 @@ from .api_client_common import APIClientCommon  # noqa: F401
 from .auth_method.auth_basic import BasicAuthAPIClient  # noqa: F401
 from .auth_method.auth_bearer import BearerAuthAPIClient  # noqa: F401
 from .auth_method.auth_sharedKeyLite import SharedKeyLiteAuthAPIClient  # noqa: F401
+from .auth_method.auth_imap import IMAPAuthClient  # noqa: F401

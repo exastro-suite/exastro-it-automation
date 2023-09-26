122baf59d7a66c654dd101ab7de0ca77db832aeb
h-nanri <hibiki.nanri@lean-jp.com>
2023-09-26 10:34:16 +0900

    #1410

--- ita_root/ita_by_menu_create/backyard_main.py
+++ ita_root/ita_by_menu_create/backyard_main.py
@@ -33,6 +33,18 @@ def backyard_main(organization_id, workspace_id):
     debug_msg = g.appmsg.get_log_message("BKY-20001", [])
     g.applogger.debug(debug_msg)
 
+    # メンテナンスモードのチェック
+    try:
+        maintenance_mode = get_maintenance_mode_setting()
+        # data_update_stopの値が"1"の場合、メンテナンス中のためreturnする。
+        if str(maintenance_mode['data_update_stop']) == "1":
+            g.applogger.debug(g.appmsg.get_log_message("BKY-00005", []))
+            return
+    except Exception:
+        # エラーログ出力
+        g.applogger.error(g.appmsg.get_log_message("BKY-00008", []))
+        return
+
     # テーブル名
     t_menu_create_history = 'T_MENU_CREATE_HISTORY'  # パラメータシート作成履歴
 
@@ -58,16 +70,9 @@ def backyard_main(organization_id, workspace_id):
             continue
         objdbca.db_transaction_end(True)
 
-    # メンテナンスモード「backyard_execute_stop」が有効(1)の場合は処理を終了する。
-    try:
-        maintenance_mode = get_maintenance_mode_setting()
-        # backyard_execute_stopの値が"1"の場合、メンテナンス中のためreturnする。
-        if str(maintenance_mode['backyard_execute_stop']) == "1":
-            g.applogger.debug(g.appmsg.get_log_message("BKY-00006", []))
-            return
-    except Exception:
-        # エラーログ出力
-        g.applogger.error(g.appmsg.get_log_message("BKY-00008", []))
+    # backyard_execute_stopの値が"1"の場合、メンテナンス中のためreturnする。
+    if str(maintenance_mode['backyard_execute_stop']) == "1":
+        g.applogger.debug(g.appmsg.get_log_message("BKY-00006", []))
         return
 
     # 「パラメータシート作成履歴」から「未実行(ID:1)」のレコードを取得

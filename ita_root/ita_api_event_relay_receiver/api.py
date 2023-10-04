f168ef0d2e34cc5a097fa17f498fccdc47a2dad8
uekawa-tomonari <16901713+uekawa-tomonari@users.noreply.github.com>
2023-10-04 09:31:22 +0900

    ita-api-event-relay-receiverコンテナと通信

--- ita_root/ita_api_event_relay_receiver/api.py
+++ ita_root/ita_api_event_relay_receiver/api.py
@@ -36,6 +36,6 @@ if __name__ == '__main__':
     flask_app.run(
         debug=True,
         host='0.0.0.0',
-        port=int(os.environ.get('LISTEN_PORT', '8079')),
+        port=int(os.environ.get('LISTEN_PORT', '8000')),
         threaded=True
     )

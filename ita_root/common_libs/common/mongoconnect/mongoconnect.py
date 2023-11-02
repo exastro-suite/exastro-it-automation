5b00a032d5f2eadedf397295752696ecd123642e
r-gima <r-gima@ncontr.com>
2023-11-02 14:26:33 +0900

    exastro-suite#1411 必要な個所でのみpymongoを利用するように定義を見直し

--- ita_root/common_libs/common/mongoconnect/mongoconnect.py
+++ ita_root/common_libs/common/mongoconnect/mongoconnect.py
@@ -232,26 +232,7 @@ class MONGOConnectWs(MONGOConnectRoot):
         # connect database
         self.connect()
 
-
-class CollectionFactory():
-    """
-    CollectionFactory
-
-        CollectionBaseの具象クラスをコレクション名を与えて生成するために定義したクラス。
-        定義のメンテナンスを行う際は以下を確認すること。
-        ・CollectionFactory.FACTORY_MAP
-        ・collection/__init__.py
-        ・const.py
-
-    """
-
-    # MongoDBのコレクション名と対応するクラスの対応表
-    FACTORY_MAP: [str, CollectionBase] = {
-        Const.LABELED_EVENT_COLLECTION: collection.LabeledEventCollection
-    }
-
-    @classmethod
-    def create(cls, collection_name: str) -> CollectionBase:
+    def create_collection(self, collection_name: str):
         '''
         コレクション名に対応したCollectionBaseの具象クラスを生成する。
         Arguments:
@@ -264,20 +245,65 @@ class CollectionFactory():
             CollectionBaseの具象クラス
         '''
 
-        if collection_name in cls.FACTORY_MAP:
-            return cls.FACTORY_MAP[collection_name]()
-        raise TypeError
+        return self.CollectionFactory.create(collection_name)
 
-    @classmethod
-    def get_collection_name(cls, table_name: str) -> str:
+    def get_collection_name(self, table_name):
         """
         MariaDBのテーブル名に対応するMongoDBのコレクション名を返却する
 
         Arguments:
-            mariadb_table_name (str): テーブル名
+            table_name (str): MariaDBのテーブル名
 
         Returns:
             str: コレクション名
         """
+        return self.CollectionFactory.get_collection_name(table_name)
 
-        return Const.NAME_MAP[table_name]
+    class CollectionFactory():
+        """
+        CollectionFactory
+        利便性を考慮してMONGOConnectWsのインナークラスとして定義
+            CollectionBaseの具象クラスをコレクション名を与えて生成するために定義したクラス。
+            定義のメンテナンスを行う際は以下を確認すること。
+            ・CollectionFactory.FACTORY_MAP
+            ・collection/__init__.py
+            ・const.py
+
+        """
+
+        # MongoDBのコレクション名と対応するクラスの対応表
+        FACTORY_MAP: [str, CollectionBase] = {
+            Const.LABELED_EVENT_COLLECTION: collection.LabeledEventCollection
+        }
+
+        @classmethod
+        def create(cls, collection_name: str) -> CollectionBase:
+            '''
+            コレクション名に対応したCollectionBaseの具象クラスを生成する。
+            Arguments:
+                collection_name: 生成するクラスに対応したコレクション名
+
+            Raises:
+                TypeError:未定義のコレクション名を受け取った場合に発生
+
+            Returns:
+                CollectionBaseの具象クラス
+            '''
+
+            if collection_name in cls.FACTORY_MAP:
+                return cls.FACTORY_MAP[collection_name]()
+            raise TypeError
+
+        @classmethod
+        def get_collection_name(cls, table_name: str) -> str:
+            """
+            MariaDBのテーブル名に対応するMongoDBのコレクション名を返却する
+
+            Arguments:
+                table_name (str): テーブル名
+
+            Returns:
+                str: コレクション名
+            """
+
+            return Const.NAME_MAP[table_name]

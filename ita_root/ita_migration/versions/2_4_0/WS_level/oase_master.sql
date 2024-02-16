1e9e47cf74deca5eaabeeae3063bc2acf9c9e78a
tomoka-abe <abe-tomoka@ncontr.com>
2024-02-16 17:26:40 +0900

    #2132 migrationの追加

---
+++ ita_root/ita_migration/versions/2_4_0/WS_level/oase_master.sql
@@ -0,0 +1,40 @@
+-- INSERT/UPDATE: -2.4.0
+    -- T_COMN_MENU_COLUMN_LINK : UPDATE
+
+-- T_COMN_MENU_COLUMN_LINK: UPDATE
+UPDATE T_COMN_MENU_COLUMN_LINK SET COLUMN_NAME_JA = '接続先', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET COLUMN_NAME_JA = '接続先', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK SET COLUMN_NAME_EN = 'Access Point', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET COLUMN_NAME_EN = 'Access Point', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_JA = '[最大長]1024バイト
+接続先を入力します。
+
+以下のように入力してください。
+メールサーバの場合
+　→ホスト名を入力。ex.example.com
+APIの場合
+　→URLを入力。ex.http://example.com/dir/', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_JA = '[最大長]1024バイト
+接続先を入力します。
+
+以下のように入力してください。
+メールサーバの場合
+　→ホスト名を入力。ex.example.com
+APIの場合
+　→URLを入力。ex.http://example.com/dir/', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_EN = '[Maximum length] 1024 bytes
+Enter the connection destination.
+
+Please enter as below.
+For mail servers
+　→Enter the host name. ex.example.com
+For API
+　→Enter the URL. ex.http://example.com/dir/', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_EN = '[Maximum length] 1024 bytes
+Enter the connection destination.
+
+Please enter as below.
+For mail servers
+　→Enter the host name. ex.example.com
+For API
+　→Enter the URL. ex.http://example.com/dir/ Point', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010105';

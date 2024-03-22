90d57ca4488b1c83ec51a25e1f86d8c1a0eed2d1
enoman <c_man_enomoto@ncontr.com>
2024-03-22 15:43:34 +0900

    #1044

--- ita_root/ita_api_admin/libs/admin_common.py
+++ ita_root/ita_api_admin/libs/admin_common.py
@@ -47,7 +47,7 @@ def before_request_handler():
         # request-header check
         # ヘルスチェック用のURLの場合にUser-IdとRolesを確認しない
         url = request.url
-        ret = re.search("/internal-api/health-check/liveness$|/internal-api/health-check/readness$", url)
+        ret = re.search("/internal-api/health-check/liveness$|/internal-api/health-check/readiness$", url)
         if ret is None:
             user_id = request.headers.get("User-Id")
             roles_org = request.headers.get("Roles")

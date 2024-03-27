4a5675e499e2d538e48dafc91a6b88254073b269
koujiSunakawa <koji-sunakawa@ncontr.com>
2024-03-27 16:21:05 +0900

    #2093

--- ita_root/common_libs/oase/auth_method/auth_sharedKeyLite.py
+++ ita_root/common_libs/oase/auth_method/auth_sharedKeyLite.py
@@ -9,6 +9,8 @@ class SharedKeyLiteAuthAPIClient(APIClientCommon):
     def call_api(self, parameter):
         self.current_datetime = datetime.datetime.utcnow()
         self.fomatted_datetime = self.current_datetime.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        if self.headers is None:
+            self.headers = {}
         self.headers["Authorization"] = f"SharedKeyLite {self.access_key_id}:{self.secret_access_key}"
         self.headers["Date"] = self.fomatted_datetime
         return super().call_api(parameter)

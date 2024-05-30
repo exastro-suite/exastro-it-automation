fa7145cabf1531ce838ee2d9be7fdfd79f577039
tomoka-abe <abe-tomoka@ncontr.com>
2024-05-30 15:54:15 +0900

    #2163 各メニューソートキーの修正

---
+++ ita_root/ita_migration/versions/2_5_0/WS_level/sql/terraform_cli_master.sql
@@ -0,0 +1,17 @@
+-- INSERT/UPDATE: 2.5.0
+    -- T_COMN_MENU : UPDATE
+
+UPDATE T_COMN_MENU SET SORT_KEY = '', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90101';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90101';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"ASC":"iteration_member_variable_name"}, {"ASC":"variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90106';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"ASC":"iteration_member_variable_name"}, {"ASC":"variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90106';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"DESC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"movement_name"},{"ASC":"column_substitution_order"},{"ASC":"menu_group_menu_item"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90107';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"DESC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"movement_name"},{"ASC":"column_substitution_order"},{"ASC":"menu_group_menu_item"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90107';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"ASC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"operation"},{"ASC":"execution_no"},{"ASC":"last_update_date_time"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90111';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"ASC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"operation"},{"ASC":"execution_no"},{"ASC":"last_update_date_time"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90111';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"ASC":"member_variable_name"},{"ASC":"key_ofchild_member_variable"},{"ASC":"parent_variable_name"}, {"ASC":"original_variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90113';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"ASC":"member_variable_name"},{"ASC":"key_ofchild_member_variable"},{"ASC":"parent_variable_name"}, {"ASC":"original_variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='90113';
---
+++ ita_root/ita_migration/versions/2_5_0/WS_level/sql/terraform_cloud_ep_master.sql
@@ -0,0 +1,17 @@
+-- INSERT/UPDATE: 2.5.0
+    -- T_COMN_MENU : UPDATE
+
+UPDATE T_COMN_MENU SET SORT_KEY = '', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80101';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80101';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"ASC":"iteration_member_variable_name"}, {"ASC":"variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80111';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"ASC":"iteration_member_variable_name"}, {"ASC":"variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80111';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"DESC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"movement_name"},{"ASC":"column_substitution_order"},{"ASC":"menu_group_menu_item"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80112';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"DESC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"movement_name"},{"ASC":"column_substitution_order"},{"ASC":"menu_group_menu_item"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80112';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"ASC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"operation"},{"ASC":"execution_no"},{"ASC":"last_update_date_time"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80116';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"ASC":"substitution_order"},{"ASC":"member_variable_name"},{"ASC":"variable_name"},{"ASC":"operation"},{"ASC":"execution_no"},{"ASC":"last_update_date_time"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80116';
+
+UPDATE T_COMN_MENU SET SORT_KEY = '[{"ASC":"member_variable_name"},{"ASC":"key_ofchild_member_variable"},{"ASC":"parent_variable_name"}, {"ASC":"original_variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80119';
+UPDATE T_COMN_MENU_JNL SET SORT_KEY = '[{"ASC":"member_variable_name"},{"ASC":"key_ofchild_member_variable"},{"ASC":"parent_variable_name"}, {"ASC":"original_variable_name"}]', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE MENU_ID ='80119';

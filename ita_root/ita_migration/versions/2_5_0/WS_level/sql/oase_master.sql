a3308a911d4adfdf4c31b9f0809bfe18bd88ea41
tomoka-abe <abe-tomoka@ncontr.com>
2024-06-19 15:11:19 +0900

    #2457 migrationの追加

--- ita_root/ita_migration/versions/2_5_0/WS_level/sql/oase_master.sql
+++ ita_root/ita_migration/versions/2_5_0/WS_level/sql/oase_master.sql
@@ -1,7 +1,19 @@
 -- INSERT/UPDATE: -2.5.0
     -- T_COMN_MENU_COLUMN_LINK: UPDATE
+    -- T_COMN_MENU: UPDATE
 
 -- T_COMN_MENU_COLUMN_LINK: UPDATE
+UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_JA = '[最大長]255バイト
+どのルールから作成された結論イベントなのかを、恒久的に判別するため"_exastro_rule_name"ラベルに設定する任意の名前を入力します。
+※編集不可', DESCRIPTION_EN = '[Maximum length] 255 bytes
+Enter any name you want to set in the "_exastro_rule_name" label to permanently identify which rule the conclusion event was created from.
+* Unable to edit', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010904';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_JA = '[最大長]255バイト
+どのルールから作成された結論イベントなのかを、恒久的に判別するため"_exastro_rule_name"ラベルに設定する任意の名前を入力します。
+※編集不可', DESCRIPTION_EN = '[Maximum length] 255 bytes
+Enter any name you want to set in the "_exastro_rule_name" label to permanently identify which rule the conclusion event was created from.
+* Unable to edit', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11010904';
+
 UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_JA = '[元データ]Conductor作業履歴', DESCRIPTION_EN = '[Original data]Conductor history', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11011007';
 UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_JA = '[元データ]Conductor作業履歴', DESCRIPTION_EN = '[Original data]Conductor history', LAST_UPDATE_TIMESTAMP = _____DATE_____ WHERE COLUMN_DEFINITION_ID = '11011007';
 

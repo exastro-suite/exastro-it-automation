c392dc6eec4418c65c0395a1eda5f5f59f4b84e1
k-tomita1 <kyohei.tomita@ncontr.com>
2024-08-23 14:00:23 +0900

    #89

---
+++ ita_root/ita_api_ansible_execution_receiver/api.py
@@ -0,0 +1,41 @@
+#   Copyright 2022 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import os
+import connexion
+
+from dotenv import load_dotenv  # python-dotenv
+from libs.ansible_execution_receiver_common import before_request_handler
+# import secrets
+# import string
+
+
+# load environ variables
+load_dotenv(override=True)
+
+flask_app = connexion.FlaskApp(__name__, specification_dir='./swagger/')
+app = flask_app.app
+flask_app.add_api('swagger.yaml')
+
+flask_app.app.before_request(before_request_handler)
+# flask_app.app.secret_key = "".join(secrets.choice(string.ascii_letters + string.digits + '_' + '-' + '!' + '#' + '&') for i in range(64))
+
+
+if __name__ == '__main__':
+    flask_app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=int(os.environ.get('LISTEN_PORT', '8000')),
+        threaded=True
+    )
---
+++ ita_root/ita_api_ansible_execution_receiver/controllers/ansible_execution_controller.py
@@ -0,0 +1,209 @@
+#   Copyright 2024 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import connexion
+
+from common_libs.common import *  # noqa: F403
+from common_libs.common.dbconnect import DBConnectWs
+from common_libs.api import api_filter, api_filter_download_file, check_request_body, check_request_body_key
+from libs.ansible_execution import * # noqa: F403
+from flask import g
+
+
+@api_filter
+def get_unexecuted_instance(organization_id, workspace_id):  # noqa: E501
+    """get_unexecuted_instance
+
+    未実行インスタンス確認 # noqa: E501
+
+    :param organization_id: OrganizationID
+    :type organization_id: str
+    :param workspace_id: WorkspaceID
+    :type workspace_id: str
+
+    :rtype: InlineResponse2006
+    """
+
+    # DB接続
+    objdbca = DBConnectWs(workspace_id)  # noqa: F405
+
+    try:
+
+        # 作業実行関連のメニューの基本情報および項目情報の取得
+        result_data = unexecuted_instance(objdbca)
+        # result_data.setdefault("menu_info", tmp_data[0]["data"])
+    except Exception as e:
+        raise e
+    finally:
+        objdbca.db_disconnect()
+    return result_data,
+
+
+@api_filter
+def execution_status_notification(organization_id, workspace_id, execution_no, body):  # noqa: E501
+    """get_unexecuted_instance
+
+    作業状態通知 # noqa: E501
+
+    :param organization_id: OrganizationID
+    :type organization_id: str
+    :param workspace_id: WorkspaceID
+    :type workspace_id: str
+
+    :rtype: InlineResponse2006
+    """
+
+    # DB接続
+    objdbca = DBConnectWs(workspace_id)  # noqa: F405
+
+    try:
+        # bodyのjson形式チェック
+        check_request_body()
+
+        if connexion.request.is_json:
+            body = dict(connexion.request.get_json())
+            check_request_body_key(body, 'driver_id')  # keyが無かったら400-00002エラー
+            check_request_body_key(body, 'status')
+
+        # 作業実行関連のメニューの基本情報および項目情報の取得
+        result_data = get_execution_status(objdbca, execution_no, body)
+        # result_data.setdefault("menu_info", tmp_data[0]["data"])
+    except Exception as e:
+        raise e
+    finally:
+        objdbca.db_disconnect()
+    return result_data,
+
+
+@api_filter_download_file
+def get_populated_data(organization_id, workspace_id, execution_no, driver_id):  # noqa: E501
+    """get_populated_data
+
+    投入データ取得 # noqa: E501
+
+    :param organization_id: OrganizationID
+    :type organization_id: str
+    :param workspace_id: WorkspaceID
+    :type workspace_id: str
+    :param execution_no: 作業番号
+    :type execution_no: str
+
+    :rtype: InlineResponse2006
+    """
+
+    # DB接続
+    objdbca = DBConnectWs(workspace_id)  # noqa: F405
+
+    try:
+        # 作業実行関連のメニューの基本情報および項目情報の取得
+        result_data = get_populated_data_path(objdbca, organization_id, workspace_id, execution_no, driver_id)
+        # result_data.setdefault("menu_info", tmp_data[0]["data"])
+    except Exception as e:
+        raise e
+    finally:
+        objdbca.db_disconnect()
+    return result_data,
+
+@api_filter_download_file
+def update_result_data(organization_id, workspace_id, execution_no, body):  # noqa: E501
+    """update_result_data
+
+    結果データ更新 # noqa: E501
+
+    :param organization_id: OrganizationID
+    :type organization_id: str
+    :param workspace_id: WorkspaceID
+    :type workspace_id: str
+    :param execution_no: 作業番号
+
+    :rtype: InlineResponse2006
+    """
+
+    # DB接続
+    objdbca = DBConnectWs(workspace_id)  # noqa: F405
+
+    try:
+        # bodyのjson形式チェック
+        check_request_body()
+
+        if connexion.request.is_json:
+            body = dict(connexion.request.get_json())
+            check_request_body_key(body, 'driver_id')  # keyが無かったら400-00002エラー
+            check_request_body_key(body, 'status')
+
+        # 作業実行関連のメニューの基本情報および項目情報の取得
+        result_data = update_result_data(objdbca, organization_id, workspace_id, execution_no, body)
+        # result_data.setdefault("menu_info", tmp_data[0]["data"])
+    except Exception as e:
+        raise e
+    finally:
+        objdbca.db_disconnect()
+    return result_data,
+
+
+@api_filter
+def agent_version(organization_id, workspace_id):  # noqa: E501
+    """update_result_data
+
+    バージョン通知 # noqa: E501
+
+    :param organization_id: OrganizationID
+    :type organization_id: str
+    :param workspace_id: WorkspaceID
+    :type workspace_id: str
+
+    :rtype: InlineResponse2006
+    """
+
+    # DB接続
+    objdbca = DBConnectWs(workspace_id)  # noqa: F405
+
+    try:
+
+        # エージェント名とバージョン情報を取得
+        result_data = get_agent_version(objdbca)
+        # result_data.setdefault("menu_info", tmp_data[0]["data"])
+    except Exception as e:
+        raise e
+    finally:
+        objdbca.db_disconnect()
+    return result_data,
+
+@api_filter
+def execution_notification(organization_id, workspace_id):  # noqa: E501
+    """update_result_data
+
+    バージョン通知 # noqa: E501
+
+    :param organization_id: OrganizationID
+    :type organization_id: str
+    :param workspace_id: WorkspaceID
+    :type workspace_id: str
+
+    :rtype: InlineResponse2006
+    """
+
+    # DB接続
+    objdbca = DBConnectWs(workspace_id)  # noqa: F405
+
+    try:
+
+        # エージェント名とバージョン情報を取得
+        result_data = get_agent_version(objdbca, organization_id, workspace_id)
+        # result_data.setdefault("menu_info", tmp_data[0]["data"])
+    except Exception as e:
+        raise e
+    finally:
+        objdbca.db_disconnect()
+    return result_data,

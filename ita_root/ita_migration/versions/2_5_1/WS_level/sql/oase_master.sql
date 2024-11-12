a6adfbba5533c6b5cf6ddf309b05066380aea186
t-uekawa-n <uekawa.tomonari@ncontr.com>
2024-11-12 18:09:12 +0900

    #2103 openprj2224

---
+++ ita_root/ita_migration/versions/2_5_1/WS_level/sql/oase_master.sql
@@ -0,0 +1,17 @@
+UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_JA = '[最大サイズ]2MB
+テンプレートを登録します。', DESCRIPTION_EN='[Maximum size] 2MB
+Register the notification template.', LAST_UPDATE_TIMESTAMP = _____DATE_____
+ WHERE COLUMN_DEFINITION_ID = '11010909' AND COLUMN_NAME_REST = 'before_notification';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_JA = '[最大サイズ]2MB
+テンプレートを登録します。', DESCRIPTION_EN='[Maximum size] 2MB
+Register the notification template.', LAST_UPDATE_TIMESTAMP = _____DATE_____
+ WHERE COLUMN_DEFINITION_ID = '11010909' AND COLUMN_NAME_REST = 'before_notification';
+
+UPDATE T_COMN_MENU_COLUMN_LINK SET DESCRIPTION_JA = '[最大サイズ]2MB
+テンプレートを登録します。', DESCRIPTION_EN='[Maximum size] 2MB
+Register the notification template.', LAST_UPDATE_TIMESTAMP = _____DATE_____
+ WHERE COLUMN_DEFINITION_ID = '11010913' AND COLUMN_NAME_REST = 'after_notification';
+UPDATE T_COMN_MENU_COLUMN_LINK_JNL SET DESCRIPTION_JA = '[最大サイズ]2MB
+テンプレートを登録します。', DESCRIPTION_EN='[Maximum size] 2MB
+Register the notification template.', LAST_UPDATE_TIMESTAMP = _____DATE_____
+ WHERE COLUMN_DEFINITION_ID = '11010913' AND COLUMN_NAME_REST = 'after_notification';

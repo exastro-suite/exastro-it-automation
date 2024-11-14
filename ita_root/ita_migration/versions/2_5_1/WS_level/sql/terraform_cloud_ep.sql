82146b25a21fe32991f5ffb3be2c215b742a1fe6
t-uekawa-n <uekawa.tomonari@ncontr.com>
2024-11-14 11:39:13 +0900

    #2607

--- ita_root/ita_migration/versions/2_5_1/WS_level/sql/terraform_cloud_ep.sql
+++
@@ -1,6 +0,0 @@
--- ------------------------------------------------------------
--- - ▼ 代入値自動登録
--- -    MENU_NAME_REST:メニュー名(Rest)追加
--- ------------------------------------------------------------
-ALTER TABLE T_TERE_VALUE_AUTOREG     ADD  COLUMN MENU_NAME_REST VARCHAR(40) AFTER VALUE_AUTOREG_ID;
-ALTER TABLE T_TERE_VALUE_AUTOREG_JNL ADD  COLUMN MENU_NAME_REST VARCHAR(40) AFTER VALUE_AUTOREG_ID;

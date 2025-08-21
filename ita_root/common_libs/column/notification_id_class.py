baf2a50e27a5f2a6b52e2f988bc4605780932322
t-uekawa-n <150503372+t-uekawa-n@users.noreply.github.com>
2025-08-22 01:15:46 +0900

    通知テンプレートのメニューに通知先カラムを追加 #2798 フィルタでエージェントのバージョンも選択可能にする #2792

--- ita_root/common_libs/column/notification_id_class.py
+++ ita_root/common_libs/column/notification_id_class.py
@@ -33,8 +33,176 @@ class NotificationIDColumn(MultiSelectIDColumn):
                 データリスト
         """
         values = Notification.fetch_notification_destination_dict()
+        # values["1"] = "default"
 
         self.data_list_set_flg = True
 
         return values
 
+    # [load_table] 値を出力用の値へ変換
+    def convert_value_output(self, val=''):
+        """
+            値を出力用の値へ変換
+            ARGS:
+                val:値
+            RETRUN:
+                retBool, msg, val
+        """
+        msg = ''
+
+        try:
+            if not val:
+                val = {}
+                val['id'] = []
+                val = json.dumps(val)
+            json_val = json.loads(val)
+            if type(json_val["id"]) != list:
+                status_code = 'MSG-00001'
+                exp_args = "Not in JSON list format data(%s)" % (str(val))
+                raise Exception(exp_args)
+        except Exception as e:
+            # エラーでリターンの処理がないのでException
+            print_exception_msg(e)
+            status_code = '499-01708'
+            msg_args = [str(val)]
+            msg = g.appmsg.get_api_message(status_code, msg_args)
+            raise Exception(status_code, msg)
+
+        master_row = self.get_values_by_key(json_val["id"])
+
+        search_candidates = []
+        if isinstance(json_val["id"], list):
+            keys = json_val["id"]
+            for key in keys:
+                if key in master_row.keys():
+                    val = master_row[key]
+                    if not search_candidates.count(val):
+                        search_candidates.append(val)
+                else:
+                    if key == "1":
+                    # OASE管理->通知テンプレート(共通)の、デフォルトレコード用
+                        search_candidates.append("default")
+                    else:
+                        # 廃止されているデータをID変換失敗(key)として扱う
+                        val = g.appmsg.get_api_message('MSG-00001', [key])
+                        if not search_candidates.count(val):
+                            search_candidates.append(val)
+        else:
+            status_code = 'MSG-00001'
+            msg_args = ["JSON format error(not list) data(%s)" % (str(val))]
+            val = g.appmsg.get_api_message(status_code, msg_args)
+            return False, msg, val,
+
+        # 全角文字対応　ensure_ascii=False
+        return True, msg, json.dumps(search_candidates, ensure_ascii=False),
+
+    # [load_table] 値を入力用の値へ変換
+    def convert_value_input(self, valnames=''):
+        """
+            値を入力用の値へ変換
+            ARGS:
+                vallist:値 "key name1, ..."
+            RETRUN:
+                retBool, msg, val
+        """
+        retBool = True
+        retdict = {"id": []}
+        msg = ''
+        if not valnames:
+            return True, '', valnames,
+        val_decode = self.is_json_format(valnames)
+        if val_decode is False:
+            return False, 'JSON format is abnormal', valnames,
+        if len(val_decode) == 0:
+            return True, '', None,
+
+        if val_decode is not None:
+            for val in val_decode:
+                return_values = self.get_values_by_value([val]) if val != "default" else {"1": "default"}
+                print(return_values)
+
+                if len(return_values) == 1:
+                    # val = list(return_values.keys())[0]
+                    retdict["id"].append(list(return_values.keys())[0])
+                    # ユニーク判定をする為に、Key値をソートしてDBに保存
+                    retdict["id"].sort()
+                else:
+                    retBool = False
+                    status_code = 'MSG-00032'
+                    msg_args = [val]
+                    msg = g.appmsg.get_api_message(status_code, msg_args)
+                    return retBool, msg, val,
+
+        return retBool, msg, json.dumps(retdict),
+
+    def check_basic_valid(self, valnames, option={}):
+        """
+            バリデーション処理(カラムクラス毎)
+            ARGS:
+                val:値
+                option:オプション
+            RETRUN:
+                ( True / False , メッセージ )
+        """
+        retBool = True
+
+        val_decode = None
+        if valnames:
+            val_decode = self.is_json_format(valnames)
+            if val_decode is False:
+                print_exception_msg("JSON format is abnormal")
+                retBool = False
+                status_code = '499-01701'
+                msg_args = []
+                msg = g.appmsg.get_api_message(status_code, msg_args)
+                return retBool, msg
+
+            set_Valses = []
+            if len(val_decode) > 0:
+                for val in val_decode:
+                    return_values = self.get_values_by_value([val]) if val != "default" else {"1": "default"}
+                    # 返却値が存在するか確認
+                    if len(return_values) == 0:
+                        retBool = False
+                        status_code = 'MSG-00032'
+                        msg_args = [val]
+                        msg = g.appmsg.get_api_message(status_code, msg_args)
+                        return retBool, msg
+
+                    # 同じ値が複数あるか判定
+                    if set_Valses.count(return_values) > 0:
+                        retBool = False
+                        status_code = '499-01702'
+                        msg_args = [val]
+                        msg = g.appmsg.get_api_message(status_code, msg_args)
+                        return retBool, msg
+                    set_Valses.append(return_values)
+
+        min_length = 0
+        max_length = None
+
+        if val_decode:
+            # カラムの閾値を取得
+            objcols = self.get_objcols()
+            if objcols is not None:
+                if self.get_rest_key_name() in objcols:
+                    dict_valid = self.get_dict_valid()
+                    # 閾値(文字列長)
+                    max_length = dict_valid.get('max_length')
+                    min_length = dict_valid.get('min_length')
+                    if min_length is None:
+                        min_length = 0
+                    if max_length is None:
+                        min_length = 1024 * 10
+            # 文字列長
+            if max_length is not None:
+                check_val = len(str(val_decode).encode('utf-8'))
+                if check_val != 0:
+                    if int(min_length) <= check_val <= int(max_length):
+                        retBool = True
+                    else:
+                        retBool = False
+                        msg = g.appmsg.get_api_message('MSG-00008', [max_length, check_val])
+                        return retBool, msg
+
+        return retBool,

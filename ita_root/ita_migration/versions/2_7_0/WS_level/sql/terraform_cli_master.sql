b84d5f3b8bc333c886ae94d3d0ce7a91cbecefe7
t-uekawa-n <150503372+t-uekawa-n@users.noreply.github.com>
2025-09-18 01:09:40 +0900

    バージョンアップと新規のワークスペースで、sqlの差分をなくしたい #2821

--- ita_root/ita_migration/versions/2_7_0/WS_level/sql/terraform_cli_master.sql
+++ ita_root/ita_migration/versions/2_7_0/WS_level/sql/terraform_cli_master.sql
@@ -3,7 +3,7 @@
 -- -----------------------------------------------------------------------------
 UPDATE T_COMN_MENU_TABLE_LINK      SET MENU_INFO_EN = 'You can perform maintenance (view/update) for connection interface information.
 This menu should be one record.'  WHERE TABLE_DEFINITION_ID = '90101';
-UPDATE T_COMN_MENU_TABLELINK_JNL   SET MENU_INFO_EN = 'You can perform maintenance (view/update) for connection interface information.
+UPDATE T_COMN_MENU_TABLE_LINK_JNL   SET MENU_INFO_EN = 'You can perform maintenance (view/update) for connection interface information.
 This menu should be one record.'  WHERE TABLE_DEFINITION_ID = '90101';
 
 UPDATE T_COMN_MENU_TABLE_LINK      SET MENU_INFO_EN = 'You can perform maintenance (view/register/update/discard) for operations registered in the associated menu, and Movement and variables associated with the setting value of item.
@@ -11,7 +11,7 @@ UPDATE T_COMN_MENU_TABLE_LINK      SET MENU_INFO_EN = 'You can perform maintenan
 There are two methods to register the setting value of item.
 Value type: Setting value of item is registered in the substitution value list as a specific value of associated variable.
 Key type: Item name is registered in the substitution value list as a specific value of associated variable. '  WHERE TABLE_DEFINITION_ID = '90107';
-UPDATE T_COMN_MENU_TABLELINK_JNL   SET MENU_INFO_EN = 'You can perform maintenance (view/register/update/discard) for operations registered in the associated menu, and Movement and variables associated with the setting value of item.
+UPDATE T_COMN_MENU_TABLE_LINK_JNL   SET MENU_INFO_EN = 'You can perform maintenance (view/register/update/discard) for operations registered in the associated menu, and Movement and variables associated with the setting value of item.
 
 There are two methods to register the setting value of item.
 Value type: Setting value of item is registered in the substitution value list as a specific value of associated variable.
@@ -21,7 +21,7 @@ UPDATE T_COMN_MENU_TABLE_LINK      SET MENU_INFO_EN = 'The following Stand alone
 ・Immediate execution
 ・Scheduled execution
 Select Movement and Operation ID to execute.'  WHERE TABLE_DEFINITION_ID = '90108';
-UPDATE T_COMN_MENU_TABLELINK_JNL   SET MENU_INFO_EN = 'The following Stand alone Movement executions are possible.
+UPDATE T_COMN_MENU_TABLE_LINK_JNL   SET MENU_INFO_EN = 'The following Stand alone Movement executions are possible.
 ・Immediate execution
 ・Scheduled execution
 Select Movement and Operation ID to execute.'  WHERE TABLE_DEFINITION_ID = '90108';

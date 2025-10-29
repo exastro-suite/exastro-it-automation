74ba038ec98913f11c7dfea5a8c750811b313327
t-uekawa-n <150503372+t-uekawa-n@users.noreply.github.com>
2025-10-30 01:46:45 +0900

    OASEの機能を最新の状態で使いたい #2837

--- ita_root/common_libs/oase/auth_method/auth_basic.py
+++ ita_root/common_libs/oase/auth_method/auth_basic.py
@@ -2,12 +2,12 @@ from common_libs.oase.api_client_common import APIClientCommon
 import base64
 
 class BasicAuthAPIClient(APIClientCommon):
-    def __init__(self, auth_settings=None):
-        super().__init__(auth_settings)
+    def __init__(self, setting, last_fetched_event):
+        super().__init__(setting, last_fetched_event)
 
-    def call_api(self, parameter):
+    def call_api(self):
         if self.headers is None:
             self.headers = {}
         base64string = base64.b64encode('{}:{}'.format(self.username, self.password).encode('utf-8'))
         self.headers["Authorization"] = "Basic " + base64string.decode('utf-8')
-        return super().call_api(parameter)
+        return super().call_api()

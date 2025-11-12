aaca4bb23c50e736baa52ec33476437d3bdaa943
KaNakayama <kazutomo.nakayama@ncontr.com>
2025-11-12 22:03:07 +0900

    パターン追加

--- ita_root/ita_by_oase_conclusion/tests/test_pattern_070_079.py
+++ ita_root/ita_by_oase_conclusion/tests/test_pattern_070_079.py
@@ -32,8 +32,8 @@ def test_pattern_070(
     ws_db, mock_mongo = patch_database_connections
     mock_datetime = patch_datetime
 
-    test_events = create_events(["e001", "e004", "e005a", "e005b", "e024", "e025", "e026b", "e037"], "p070")
-    e001, e004, e005a, e005b, e024, e025, e026b, e037 = test_events
+    test_events = create_events(["e001", "e004", "e005a", "e005b", "e024", "e025", "e037"], "p070")
+    e001, e004, e005a, e005b, e024, e025, e037 = test_events
 
     filters = [f_a7, f_q_a]
     rules = [
@@ -49,15 +49,19 @@ def test_pattern_070(
 
     assert e005a["labels"]["_exastro_evaluated"] == "1"
     assert e005b["labels"]["_exastro_evaluated"] == "1"
+    assert e025["labels"]["_exastro_evaluated"] == "1"
 
-    assert e026b["labels"]["_exastro_timeout"] == "1"
+    assert e037["labels"]["_exastro_timeout"] == "1"
 
-    # TODO: 結果の確認が必要
-    import pprint
-    pprint.pprint(test_events)
+    # グルーピングの確認
+    grouped_events = [e for e in test_events if e.get("exastro_filter_group")]
+    assert len(grouped_events) == 4
 
-    assert e037["labels"]["_exastro_evaluated"] == "1"  # _exastro_timeout == "1" になっている
-    assert e025["labels"]["_exastro_timeout"] == "1"  # _exastro_evaluated == "1" になっている
+    assert e001["exastro_filter_group"]["is_first_event"] is True
+    assert e001["exastro_filter_group"]["group_id"] == e004["exastro_filter_group"]["group_id"]
+
+    assert e005a["exastro_filter_group"]["is_first_event"] is True
+    assert e005a["exastro_filter_group"]["group_id"] == e005b["exastro_filter_group"]["group_id"]
 
 
 def test_pattern_071(
@@ -87,15 +91,20 @@ def test_pattern_071(
 
     assert e014b["labels"]["_exastro_evaluated"] == "1"
     assert e014c["labels"]["_exastro_evaluated"] == "1"
+    assert e006["labels"]["_exastro_evaluated"] == "1"
+    assert e007["labels"]["_exastro_evaluated"] == "1"
 
-    # TODO: 結果の確認が必要
-    import pprint
-    pprint.pprint(test_events)
+    assert e027["labels"]["_exastro_evaluated"] == "1"
+    assert e027a["labels"]["_exastro_evaluated"] == "1"
 
-    assert e006["labels"]["_exastro_undetected"] == "1"  # _exastro_evaluated == "1" になっている
-    assert e007["labels"]["_exastro_undetected"] == "1"  # _exastro_evaluated == "1" になっている
-    assert e027["labels"]["_exastro_undetected"] == "1"  # _exastro_evaluated == "1" になっている
-    assert e027a["labels"]["_exastro_undetected"] == "1"  # _exastro_evaluated == "1" になっている
+    # グルーピングの確認
+    grouped_events = [e for e in test_events if e.get("exastro_filter_group")]
+    assert len(grouped_events) == 4
+
+    assert e014b["exastro_filter_group"]["is_first_event"] is True
+    assert e014b["exastro_filter_group"]["group_id"] == e014c["exastro_filter_group"]["group_id"]
+    assert e014b["exastro_filter_group"]["group_id"] == e006["exastro_filter_group"]["group_id"]
+    assert e014b["exastro_filter_group"]["group_id"] == e007["exastro_filter_group"]["group_id"]
 
 
 def test_pattern_072(
@@ -127,11 +136,14 @@ def test_pattern_072(
     assert e012["labels"]["_exastro_evaluated"] == "1"
     assert e025["labels"]["_exastro_evaluated"] == "1"
 
-    # TODO: 結果の確認が必要
-    import pprint
-    pprint.pprint(test_events)
+    assert e026b["labels"]["_exastro_timeout"] == "1"
 
-    assert e026b["labels"]["_exastro_evaluated"] == "1"  # _exastro_timeout == "1" になっている
+    # グルーピングの確認
+    grouped_events = [e for e in test_events if e.get("exastro_filter_group")]
+    assert len(grouped_events) == 2
+
+    assert e011["exastro_filter_group"]["is_first_event"] is True
+    assert e011["exastro_filter_group"]["group_id"] == e012["exastro_filter_group"]["group_id"]
 
 
 def test_pattern_073(
@@ -166,6 +178,14 @@ def test_pattern_073(
     assert e027["labels"]["_exastro_undetected"] == "1"
     assert e027a["labels"]["_exastro_undetected"] == "1"
 
+    # TODO: 結果の確認が必要
+    import pprint
+    pprint.pprint(test_events)
+
+    # グルーピングの確認
+    grouped_events = [e for e in test_events if e.get("exastro_filter_group")]
+    assert len(grouped_events) == 0
+
 
 def test_pattern_077(
     patch_global_g,
@@ -181,8 +201,8 @@ def test_pattern_077(
     ws_db, mock_mongo = patch_database_connections
     mock_datetime = patch_datetime
 
-    test_events = create_events(["e006", "e007", "e008", "e009", "e014a", "e014b", "e014c", "e025", "e033"], "p077")
-    e006, e007, e008, e009, e014a, e014b, e014c, e025, e033 = test_events
+    test_events = create_events(["e006", "e007", "e008", "e009", "e014a", "e014b", "e014c", "e025", "e026"], "p077")
+    e006, e007, e008, e009, e014a, e014b, e014c, e025, e026 = test_events
 
     filters = [f_a8, f_q_a]
     rules = [
@@ -192,12 +212,16 @@ def test_pattern_077(
 
     run_test_pattern(g, ws_db, mock_mongo, mock_datetime, test_events, filters, rules, actions)
 
+    assert e014a["labels"]["_exastro_timeout"] == "1"
+    assert e014b["labels"]["_exastro_timeout"] == "1"
+    assert e014c["labels"]["_exastro_timeout"] == "1"
+
     assert e006["labels"]["_exastro_evaluated"] == "1"
     assert e007["labels"]["_exastro_evaluated"] == "1"
     assert e025["labels"]["_exastro_evaluated"] == "1"
 
     assert e008["labels"]["_exastro_evaluated"] == "1"
-    assert e033["labels"]["_exastro_evaluated"] == "1"
+    assert e026["labels"]["_exastro_evaluated"] == "1"
 
     assert e009["labels"]["_exastro_undetected"] == "1"
 
@@ -205,9 +229,14 @@ def test_pattern_077(
     import pprint
     pprint.pprint(test_events)
 
-    assert e014a["labels"]["_exastro_evaluated"] == "1"  # _exastro_timeout == "1" になっている
-    assert e014b["labels"]["_exastro_evaluated"] == "1"  # _exastro_timeout == "1" になっている
-    assert e014c["labels"]["_exastro_evaluated"] == "1"  # _exastro_timeout == "1" になっている
+    # グルーピングの確認
+    grouped_events = [e for e in test_events if e.get("exastro_filter_group")]
+    assert len(grouped_events) == 3
+
+    assert e006["exastro_filter_group"]["is_first_event"] is True
+    assert e006["exastro_filter_group"]["group_id"] == e007["exastro_filter_group"]["group_id"]
+
+    assert e008["exastro_filter_group"]["is_first_event"] is True
 
 
 def test_pattern_079(
@@ -252,3 +281,15 @@ def test_pattern_079(
     pprint.pprint(test_events)
 
     assert e027a["labels"]["_exastro_evaluated"] == "1"  # _exastro_timeout == "1" になっている
+
+    # グルーピングの確認
+    grouped_events = [e for e in test_events if e.get("exastro_filter_group")]
+    assert len(grouped_events) == 3
+
+    assert e002["exastro_filter_group"]["is_first_event"] is True
+    assert e002["exastro_filter_group"]["group_id"] == e003["exastro_filter_group"]["group_id"]
+    assert e002["exastro_filter_group"]["group_id"] == e003a["exastro_filter_group"]["group_id"]
+    assert e002["exastro_filter_group"]["group_id"] == e005["exastro_filter_group"]["group_id"]
+
+    assert e005a["exastro_filter_group"]["is_first_event"] is True
+    assert e005a["exastro_filter_group"]["group_id"] == e005b["exastro_filter_group"]["group_id"]
